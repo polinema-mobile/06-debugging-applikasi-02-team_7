1cba62405019d1cddce6c045f739c485de853869
latif-abdul <54927430+latif-abdul@users.noreply.github.com>
2020-09-23 16:21:46 +0700

    Create TestKali.java

---
+++ app/src/androidTest/java/com/example/new_sample/TestKali.java
@@ -0,0 +1,32 @@
+package com.example.new_sample;
+
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class TesTambah {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void tesKlikButtonSatu(){
+        //cari id button1 terus di klik
+        onView(withId(R.id.button4)).perform(click());
+        onView(withId(R.id.button_multi)).perform(click());
+        onView(withId(R.id.button2)).perform(click());
+        onView(withId(R.id.input)).check(matches(withText("8")));
+    }
+
+}
